67bd75e12ea65118f95127dc9888a1c51b839d85
huangdong <huangdong@followme.com>
2018-05-10 13:18:40 +0800

    添加项目文件。

---
+++ HD.EFCore.SqlServerGenerator/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HD.EFCore.SqlServerGenerator.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
