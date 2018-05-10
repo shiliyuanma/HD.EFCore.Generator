9b3043b7fdc95183b77ebe7ac3daffa7ed42954c
huangdong <huangdong@followme.com>
2018-05-10 18:43:19 +0800

    搞定技术难点

--- HD.EFCore.MySqlGenerator/Controllers/HomeController.cs
+++ HD.EFCore.MySqlGenerator/Controllers/HomeController.cs
@@ -5,26 +5,32 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using HD.EFCore.MySqlGenerator.Models;
+using Microsoft.EntityFrameworkCore.Scaffolding.Internal;
+using System.IO;
 
 namespace HD.EFCore.MySqlGenerator.Controllers
 {
     public class HomeController : Controller
     {
+        IModelScaffolder _modelScaffolder;
+        public HomeController(IModelScaffolder modelScaffolder)
+        {
+            _modelScaffolder = modelScaffolder;
+        }
+
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
+            var filePaths = _modelScaffolder.Generate(
+                    "Server=localhost;Port=3306;Database=golang;Uid=root;Pwd=870224;",
+                    Enumerable.Empty<string>(),
+                    Enumerable.Empty<string>(),
+                    "Data",
+                    "",
+                    "HD.Data",
+                    contextName: "HDDbContext",
+                    useDataAnnotations: false,
+                    overwriteFiles: true,
+                    useDatabaseNames: false);
 
             return View();
         }
---
+++ HD.EFCore.MySqlGenerator/Data/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HD.Data
+{
+    public partial class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? Age { get; set; }
+    }
+}
