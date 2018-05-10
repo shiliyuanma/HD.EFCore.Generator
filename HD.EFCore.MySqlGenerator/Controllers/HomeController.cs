1a5fd65676c6a5b36df4372383528d2c505bc19d
huangdong <huangdong@followme.com>
2018-05-10 20:44:42 +0800

    完成mysql生成器的开发

--- HD.EFCore.MySqlGenerator/Controllers/HomeController.cs
+++ HD.EFCore.MySqlGenerator/Controllers/HomeController.cs
@@ -1,38 +1,62 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using HD.EFCore.MySqlGenerator.Models;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
-using HD.EFCore.MySqlGenerator.Models;
 using Microsoft.EntityFrameworkCore.Scaffolding.Internal;
+using System;
+using System.Diagnostics;
 using System.IO;
+using System.IO.Compression;
+using System.Linq;
 
 namespace HD.EFCore.MySqlGenerator.Controllers
 {
     public class HomeController : Controller
     {
+        IHostingEnvironment _env;
         IModelScaffolder _modelScaffolder;
-        public HomeController(IModelScaffolder modelScaffolder)
+        public HomeController(IHostingEnvironment env, IModelScaffolder modelScaffolder)
         {
+            _env = env;
             _modelScaffolder = modelScaffolder;
         }
 
+        [HttpGet]
         public IActionResult Index()
         {
+            return View(new SqlGenerateViewModel());
+        }
+
+        [HttpPost]
+        public IActionResult Index(SqlGenerateViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                model.ErrorMsg = string.Join("/", ModelState.Where(q => q.Value.Errors != null && q.Value.Errors.Count > 0).SelectMany(q => q.Value.Errors).Select(q => q.ErrorMessage));
+                return View(model);
+            }
+
+            var dir = Path.Combine(_env.ContentRootPath, "Data");
+            var subDir = Path.Combine(dir, DateTime.Now.ToString("yyyyMMdd-HHmmss") + "-" + new Random().Next(100, 999).ToString());
             var filePaths = _modelScaffolder.Generate(
-                    "Server=localhost;Port=3306;Database=golang;Uid=root;Pwd=870224;",
+                    model.ConnectionString,
+                    !string.IsNullOrWhiteSpace(model.TableNames)
+                        ? model.TableNames.Split(new[] { ',', '，', ' ' }, System.StringSplitOptions.RemoveEmptyEntries).ToList()
+                        : Enumerable.Empty<string>(),
                     Enumerable.Empty<string>(),
-                    Enumerable.Empty<string>(),
-                    "Data",
-                    "",
-                    "HD.Data",
-                    contextName: "HDDbContext",
+                    dir,
+                    subDir,
+                    !string.IsNullOrWhiteSpace(model.NamespaceName) ? model.NamespaceName.Trim() : "HD.EFCore.MySqlGenerator",
+                    contextName: !string.IsNullOrWhiteSpace(model.DbContextName) ? model.DbContextName.Trim() : "HDDbContext",
                     useDataAnnotations: false,
                     overwriteFiles: true,
                     useDatabaseNames: false);
 
-            return View();
+            var zipPath = $"{Path.GetFileName(subDir)}.zip";
+            ZipFile.CreateFromDirectory(subDir, zipPath);
+            var data = System.IO.File.ReadAllBytes(zipPath);
+            Directory.Delete(subDir, true);
+
+            return File(data, "application/x-zip-compressed", zipPath);
         }
 
         public IActionResult Error()
---
+++ HD.EFCore.MySqlGenerator/Models/SqlGenerateViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HD.EFCore.MySqlGenerator.Models
+{
+    public class SqlGenerateViewModel
+    {
+        [Required(ErrorMessage = "连接串不能为空")]
+        public string ConnectionString { get; set; } = "Server=localhost;Port=3306;Database=test;Uid=root;Pwd=123456;";
+
+        public string TableNames { get; set; }
+
+        public string NamespaceName { get; set; }
+
+        public string DbContextName { get; set; }
+
+        public string ErrorMsg { get; set; }
+    }
+}
